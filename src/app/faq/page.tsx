39c7ce32b3d4550e6f1823c164b7d474a1c5a155
Pustam Rai <github.pustam@gmail.com>
2025-08-09 21:46:19 +0545

    add faq page and improve width wrapper

---
+++ src/app/faq/page.tsx
@@ -0,0 +1,12 @@
+import { FaqSection } from "@/components/FAQSection";
+import WidthWrapper from "@/components/WidthWrapper";
+
+export default function FaqPage() {
+   return (
+      <WidthWrapper>
+         <main>
+            <FaqSection />
+         </main>
+      </WidthWrapper>
+   );
+}
