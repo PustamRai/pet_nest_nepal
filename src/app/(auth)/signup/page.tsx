d56f6a4befd3879bfcd7840aaac5721f80b0d6e9
Pustam Rai <github.pustam@gmail.com>
2025-08-12 23:30:06 +0545

    remove unused SignupForm import and related markup from SignupPage

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,4 @@
 import { SignupSection } from "@/components/auth/signup/signup-section";
-import { SignupForm } from "@/components/auth/SignupForm";
 import WidthWrapper from "@/components/WidthWrapper";
 import React from "react";
 
@@ -8,8 +7,6 @@ function SignupPage() {
       <WidthWrapper>
          <main>
             <SignupSection />
-         <main className="flex py-10 sm:mt-10 sm:mb-10 items-center justify-center max-w-md mx-auto">
-            <SignupForm />
          </main>
       </WidthWrapper>
    );
