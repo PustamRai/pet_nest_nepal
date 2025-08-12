16cf99a91352b907952d5d93b5f10fe2f034a308
Pustam Rai <github.pustam@gmail.com>
2025-08-12 22:28:28 +0545

    fix merge conflict in width wrapper

---
+++ src/components/WidthWrapper.tsx
@@ -0,0 +1,18 @@
+import type React from "react";
+export default function WidthWrapper({
+   children,
+   className,
+}: {
+   children: React.ReactNode;
+   className?: string;
+}) {
+   return (
+      <div
+         className={`max-w-7xl mx-auto px-2 py-5 sm:py-7 sm:px-6 lg:px-8 ${
+            className || ""
+         }`}
+      >
+         {children}
+      </div>
+   );
+}
