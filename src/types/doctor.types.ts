1c38c7cc11b337b60e8d164677fb292c4b83a9a1
Pustam Rai <github.pustam@gmail.com>
2025-08-21 12:44:07 +0545

    add seed data doctor interface

---
+++ src/types/doctor.types.ts
@@ -0,0 +1,11 @@
+export interface Doctor {
+  _id?: string;
+  name: string;
+  specialization: string;
+  email: string;
+  phone: string;
+  experinec: string;
+  location: string;
+  availableDays: string[];
+  availableTime: string[];
+}
